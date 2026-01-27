d1ac3514625a0b51aa3f10b351a6d2a89d2eecba
Maximilian Rödiger <maxroediger@googlemail.com>
2026-01-27 12:57:51 +0100

    Implemented /add_author route for GET and POST method

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 import os
 from data_models import db, Author, Book
+from datetime import datetime
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
@@ -9,9 +10,45 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(basedir, 'data
 db.init_app(app)
 
 #Create the tables defined in data_models.py, Only run when tables are not created yet. Comment out otherwise!
-with app.app_context():
-  db.create_all()
+#with app.app_context():
+#  db.create_all()
 
+@app.route('/add_author', methods=['GET', 'POST'])
+def add_author():
+    success_message = None
+    error_message = None
+
+    if request.method == 'POST':
+        name = request.form.get("name")
+        birth_date = request.form.get("birth_date")   # matches HTML
+        date_of_death = request.form.get("date_of_death")
+
+        if not name or not birth_date:
+            error_message = "Name and birth date are required."
+        else:
+            birth_date = datetime.strptime(birth_date, "%Y-%m-%d").date()
+
+            if date_of_death:
+                date_of_death = datetime.strptime(date_of_death, "%Y-%m-%d").date()
+            else:
+                date_of_death = None
+
+            new_author = Author(
+                name=name,
+                birth_date=birth_date,
+                date_of_death=date_of_death
+            )
+
+            db.session.add(new_author)
+            db.session.commit()
+
+            success_message = "Author added successfully!"
+
+    return render_template(
+        'add_author.html',
+        success_message=success_message,
+        error_message=error_message
+    )
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5001, debug=True)
